00a8282ef82678c68c601d53b29429d22886e324
Jorge Sáez <125664643+jorgesg82@users.noreply.github.com>
2025-01-26 17:31:45 +0100

    Initial commit

---
+++ Core/Src/main.cpp
@@ -0,0 +1,28 @@
+#ifndef SIM_ON
+#include "main.h"
+
+#include "lwip.h"
+#endif
+
+#include "ST-LIB.hpp"
+
+int main(void) {
+#ifdef SIM_ON
+    SharedMemory::start();
+#endif
+
+    DigitalOutput led_on(PA1);
+    STLIB::start();
+
+    Time::register_low_precision_alarm(100, [&]() { led_on.toggle(); });
+
+    while (1) {
+        STLIB::update();
+    }
+}
+
+void Error_Handler(void) {
+    ErrorHandler("HAL error handler triggered");
+    while (1) {
+    }
+}
