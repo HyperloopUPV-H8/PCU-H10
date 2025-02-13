5782792f0caa321e6b716c0424c3cd1062cbe08e
Gonzalo Sanchez <gonzalosmoya24@gmail.com>
2025-02-13 23:09:20 +0100

    make zeroing public, use define to set slope and offset

--- Core/Inc/Sensors/CurrentSensors.hpp
+++ Core/Inc/Sensors/CurrentSensors.hpp
@@ -1,16 +1,21 @@
 #pragma once
 #include "Data/Data.hpp"
+
+#define HALL_CURRENT_SENSOR_SLOPE 96.945
+#define HALL_CURRENT_SENSOR_OFFSET -157.30
+
 class CurrentSensors{
     private: 
         Data_struct *data;
-        LinearSensor<float> sensor_u_a{Pinout::CURRENT_SENSOR_U_A,Current_Sensors::slope_u_a,Current_Sensors::offset_u_a,&data->actual_current_sensor_u_a};
-        LinearSensor<float> sensor_u_b{Pinout::CURRENT_SENSOR_U_B,Current_Sensors::slope_u_b,Current_Sensors::offset_u_b,&data->actual_current_sensor_u_b};
-        LinearSensor<float> sensor_v_a{Pinout::CURRENT_SENSOR_V_A,Current_Sensors::slope_v_a,Current_Sensors::offset_v_a,&data->actual_current_sensor_v_a};
-        LinearSensor<float> sensor_v_b{Pinout::CURRENT_SENSOR_V_B,Current_Sensors::slope_v_b,Current_Sensors::offset_v_b,&data->actual_current_sensor_v_b};
-        LinearSensor<float> sensor_w_a{Pinout::CURRENT_SENSOR_W_A,Current_Sensors::slope_w_a,Current_Sensors::offset_w_a,&data->actual_current_sensor_w_a};
-        LinearSensor<float> sensor_w_b{Pinout::CURRENT_SENSOR_W_B,Current_Sensors::slope_w_b,Current_Sensors::offset_w_b,&data->actual_current_sensor_w_b};
-        void zeroing();
+        LinearSensor<float> sensor_u_a{Pinout::CURRENT_SENSOR_U_A,HALL_CURRENT_SENSOR_SLOPE,HALL_CURRENT_SENSOR_OFFSET,&data->actual_current_sensor_u_a};
+        LinearSensor<float> sensor_u_b{Pinout::CURRENT_SENSOR_U_B,HALL_CURRENT_SENSOR_SLOPE,HALL_CURRENT_SENSOR_OFFSET,&data->actual_current_sensor_u_b};
+        LinearSensor<float> sensor_v_a{Pinout::CURRENT_SENSOR_V_A,HALL_CURRENT_SENSOR_SLOPE,HALL_CURRENT_SENSOR_OFFSET,&data->actual_current_sensor_v_a};
+        LinearSensor<float> sensor_v_b{Pinout::CURRENT_SENSOR_V_B,HALL_CURRENT_SENSOR_SLOPE,HALL_CURRENT_SENSOR_OFFSET,&data->actual_current_sensor_v_b};
+        LinearSensor<float> sensor_w_a{Pinout::CURRENT_SENSOR_W_A,HALL_CURRENT_SENSOR_SLOPE,HALL_CURRENT_SENSOR_OFFSET,&data->actual_current_sensor_w_a};
+        LinearSensor<float> sensor_w_b{Pinout::CURRENT_SENSOR_W_B,HALL_CURRENT_SENSOR_SLOPE,HALL_CURRENT_SENSOR_OFFSET,&data->actual_current_sensor_w_b};
     public:
     CurrentSensors(Data_struct *data);
     void read();
+    void zeroing();
+
 };
