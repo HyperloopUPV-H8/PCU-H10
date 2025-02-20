b67f5629806a66e11efcdcd51e7717d08fc169d6
oganigl <osganig@gmail.com>
2025-02-20 16:32:03 +0100

    add new mode to calculate the peak, now it's done by doing the mean peak of the three phases

--- Core/Src/Control/CurrentControl.cpp
+++ Core/Src/Control/CurrentControl.cpp
@@ -1,5 +1,5 @@
 #include "Control/CurrentControl.hpp"
-#define NANOSECOND 1000000000
+
 CurrentControl::CurrentControl(Data_struct *Data,SpaceVector *spaceVector):
 Data(Data),
 spaceVector(spaceVector)
@@ -13,46 +13,27 @@ float CurrentControl::get_current_ref(){
     return current_ref;
 }
 double CurrentControl::calculate_peak(){
-    double tiempo = static_cast<double>(Time::get_global_tick())/NANOSECOND;
-    double maximo;
-    static double antiguo_maximo = 0.0;
-    static double t_maximo = 0.0;
-    static double posible_nuevo_maximo = 0.0;
-    static double posible_t_maximo = 0.0;
-
-   double period = 1.0 / double(spaceVector->get_modulation_frequency());
-   //Max current will be the max between the actual values and the value of the last measure
-   double max_current = std::max({std::abs(Data->actual_current_sensor_u_a), 
-                                  std::abs(Data->actual_current_sensor_u_b),
-                                  std::abs(Data->actual_current_sensor_v_a),
-                                  std::abs(Data->actual_current_sensor_v_b),
-                                  std::abs(Data->actual_current_sensor_w_a),
-                                  std::abs(Data->actual_current_sensor_w_b)}); 
-                            
-    if(max_current >= antiguo_maximo){
-        maximo = max_current;
-        antiguo_maximo = max_current;
-        t_maximo = tiempo;
-        posible_nuevo_maximo = 0.0;
-        posible_t_maximo = tiempo;
-    }else{
-        maximo = antiguo_maximo;
-        if(tiempo > (t_maximo + 0.1*period)){
-            if(max_current > posible_nuevo_maximo){
-                posible_nuevo_maximo = max_current;
-                posible_t_maximo = tiempo;
-            }
-        }
-        if(tiempo > (t_maximo + 0.6*period)){
-            maximo = posible_nuevo_maximo;
-            antiguo_maximo = posible_nuevo_maximo;
-            posible_nuevo_maximo = 0;
-            t_maximo = posible_t_maximo;
-        }
-    }
-    return maximo;
-   
-
+    Max_Peak::set_modulation_freq(spaceVector->get_modulation_frequency());
+    double total_peak;
+    #if PPU_USING != 1
+        double Peak_u_a = current_u_a.calculate_Max_Peak();
+        double Peak_v_a = current_v_a.calculate_Max_Peak();
+        double Peak_w_a = current_w_a.calculate_Max_Peak();
+        double average_a = (Peak_u_a + Peak_v_a + Peak_w_a)/3.0;
+    #endif
+    #if PPU_USING != 0
+        double Peak_u_b = current_u_b.calculate_Max_Peak();
+        double Peak_v_b = current_v_b.calculate_Max_Peak();
+        double Peak_w_b = current_W_b.calculate_Max_Peak();
+        double average_b = (Peak_u_b + Peak_v_b + Peak_w_b)/3.0;
+    #endif
+    #if PPU_USING == 0
+        return average_a;
+    #elif PPU_USING == 1
+        return average_b;
+    #elif PPU_USING == 2
+        return (average_a + average_b)/2.0;
+    #endif
 }
 void CurrentControl::control_action(){
     if (!should_be_running) return;
