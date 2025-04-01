3de511ffb60f809d82e14d4292706cf060137e00
oganigl <osganig@gmail.com>
2025-04-01 18:32:14 +0200

    look up table

--- Core/Inc/Control/SpaceVector.hpp
+++ Core/Inc/Control/SpaceVector.hpp
@@ -12,26 +12,26 @@ enum phase
 
 class SpaceVector
 {
-    private:
-        float time = 0.0;
-        Three_Phased_PWM *three_pwm;
-        float Imodulation;
-        uint32_t Modulation_frequency;
-        
-        static constexpr float IMAX = 1.1547;
+private:
+    float time = 0.0;
+    Three_Phased_PWM *three_pwm;
+    float Imodulation;
+    uint32_t Modulation_frequency;
 
-        Data_struct *data;
+    static constexpr float IMAX = 1.1547;
 
-    public:
-        float VMAX = 163.0;
+    Data_struct *data;
+
+public:
+    float VMAX = 163.0;
     static constexpr uint32_t Period = 200; // this Period is in microseconds
-        SpaceVector(Three_Phased_PWM *three_pwm_class, Data_struct *data);
-        void set_target_voltage(float V_ref);
-        void set_frequency_Modulation(uint32_t freq);
-        void calculate_duties();
-        uint32_t get_modulation_frequency();
-        float get_actual_time();
-        void set_VMAX(float VMax);
+    SpaceVector(Three_Phased_PWM *three_pwm_class, Data_struct *data);
+    void set_target_voltage(float V_ref);
+    void set_frequency_Modulation(uint32_t freq);
+    void calculate_duties();
+    uint32_t get_modulation_frequency();
+    float get_actual_time();
+    void set_VMAX(float VMax);
     #if MODE_CALCULATE_SIN == 1
         float calculate_sin_look_up_table(float angle);
         float calculate_sin_phase(phase p);
