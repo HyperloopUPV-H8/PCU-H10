55edd2bdd2d801cf326fd6601e6c2b9ac768ded2
oganigl <osganig@gmail.com>
2025-06-07 19:50:23 +0200

    add the bools to check if control is being executed in stateMachine packet

--- Core/Src/Communication/Communication.cpp
+++ Core/Src/Communication/Communication.cpp
@@ -102,7 +102,7 @@ Communication::Communication(Data_struct *data): Data(data){
     Pwm_packet  = new HeapPacket(Communication_Data::PWM_PACKET,&Data->actual_frequency,&Data->modulation_frequency,&Data->actual_duty_u,&Data->actual_duty_v,&Data->actual_duty_w);
     batteries_Packet = new HeapPacket(Communication_Data::BATTERIES_PACKET,&Data->actual_voltage_battery_A,&Data->actual_voltage_battery_B);
     Current_sensor_Packet = new HeapPacket(Communication_Data::CURRENT_SENSOR_PACKET,&Data->actual_current_sensor_u_a,&Data->actual_current_sensor_v_a,&Data->actual_current_sensor_w_a,&Data->actual_current_sensor_u_b,&Data->actual_current_sensor_v_b,&Data->actual_current_sensor_w_b,&Data->current_Peak,&Data->current_error,&Data->target_voltage,&Data->time,&Data->imod);
-    StateMachine_Packet = new HeapPacket(Communication_Data::STATE_MACHINE_PACKET,&Data->state_pcu,&Data->operational_state_pcu);
+    StateMachine_Packet = new HeapPacket(Communication_Data::STATE_MACHINE_PACKET,&Data->state_pcu,&Data->operational_state_pcu,&Data->space_vector_active,&Data->current_control_active,&Data->speed_control_active);
     Encoder_Packet = new HeapPacket(Communication_Data::ENCODER_PACKET,&Data->position_encoder,&Data->direction_encoder,&Data->speed_encoder,&Data->speed_km_h_encoder,&Data->acceleration_encoder);
     Control_Speed_Packet = new HeapPacket(Communication_Data::CONTROL_SPEED_PACKET,&Data->target_speed,&Data->speed_error,&Data->actual_current_ref);
     ControlState_Packet = new HeapPacket(Communication_Data::CONTROL_STATE_PACKET,&Data->Stablished_direction,&Data->speedState);
