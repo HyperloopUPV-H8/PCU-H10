e4c2603b8301145c06303ad791a576ffdd70fea2
Ikubimu <ikerubidemunoz@gmail.com>
2025-07-19 16:02:31 +0200

    75 amperes

--- Core/Src/StateMachinePCU/StateMachinePCU.cpp
+++ Core/Src/StateMachinePCU/StateMachinePCU.cpp
@@ -247,7 +247,7 @@ void StateMachinePCU::update(){
 
         speedControl->reset_PI();
         spaceVectorControl->set_VMAX(237);
-        currentControl->set_current_ref(80.0F);
+        currentControl->set_current_ref(75.0F);
         actuators->set_three_frequencies(20'000);
         spaceVectorControl->set_frequency_Modulation(8.3);
 
